fd3e5f8965d6bdbecad26b51da90ecc22fd9fb24
greg <gsc229@gmail.com>
2021-06-15 17:46:04 -0400

    using instanceof to type check on a class

--- simple-typescript/dist/generics/src/unions.js
+++ simple-typescript/dist/generics/src/unions.js
@@ -1,6 +1,6 @@
 "use strict";
 function someFn(myArgument) {
-    // cant do: let x = myArgument.toUpperCase()
+    // cant do: let x = myArgument.toUpperCase() <-- this is a string method, but how do we know arg is a string?
     // until you do:
     if (typeof myArgument === 'string') {
         let x = myArgument.toUpperCase(); // <-- no error
@@ -10,5 +10,39 @@ function someFn(myArgument) {
     }
     else {
         myArgument.valueOf(); // <-- same as previous 
+    }
+}
+// custom type guard
+function isDog(someObj) {
+    return someObj.bark !== undefined;
+}
+function callMyPet(pet) {
+    pet.walk(); // shared - no problem
+    // ugly way:
+    if (pet.bark) {
+        pet.bark; // (<Dog>pet) is an assertion
+    }
+    else {
+        pet.meow;
+    }
+    // better this way...
+    // using custom type guard:
+    if (isDog(pet)) {
+        pet.bark();
+    }
+    else {
+        pet.meow();
+    }
+}
+class Foo {
+}
+class Bar {
+}
+function fooBarFunction(obj) {
+    if (obj instanceof Foo) {
+        obj.foo;
+    }
+    else {
+        obj.bar;
     }
 }
--- simple-typescript/src/generics/src/unions.ts
+++ simple-typescript/src/generics/src/unions.ts
@@ -1,5 +1,5 @@
 function someFn(myArgument: number | string | boolean)  {
- // cant do: let x = myArgument.toUpperCase()
+ // cant do: let x = myArgument.toUpperCase() <-- this is a string method, but how do we know arg is a string?
  // until you do:
   if(typeof myArgument === 'string'){
     let x = myArgument.toUpperCase() // <-- no error
@@ -8,7 +8,58 @@ function someFn(myArgument: number | string | boolean)  {
   } else {
     myArgument.valueOf() // <-- same as previous 
   }
+}
 
+// The above logic extends to the case below
 
+interface Dog {
+  bark(): void
+  walk(): void
+}
 
+interface Cat {
+  meow(): void
+  walk(): void
+}
+
+// custom type guard
+function isDog(someObj: Dog | Cat): someObj is Dog {
+  return (<Dog>someObj).bark !== undefined
+}
+
+function callMyPet(pet: Dog | Cat){
+  pet.walk() // shared - no problem
+
+  // ugly way:
+  if((<Dog>pet).bark){
+    (<Dog>pet).bark // (<Dog>pet) is an assertion
+  } else{
+    (<Cat>pet).meow
+  }
+  // better this way...
+  // using custom type guard:
+  if(isDog(pet)){
+    pet.bark()
+  } else{
+    pet.meow()
+  }
+  
+}
+
+class Foo {
+  foo: number
+  commonProp: string
+}
+
+class Bar {
+  bar: number
+  commonProp: string
+}
+
+function fooBarFunction(obj: Foo | Bar) {
+  if(obj instanceof Foo){
+    obj.foo
+  } else {
+    obj.bar
+  }
 }
