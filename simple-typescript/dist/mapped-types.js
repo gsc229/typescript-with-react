91eb9b193876ae4987573252a8de56ebaa59ceb4
greg <gsc229@gmail.com>
2021-06-17 20:18:32 -0400

    Mapped Record type

---
+++ simple-typescript/dist/mapped-types.js
@@ -0,0 +1,8 @@
+"use strict";
+const someRecord = {};
+someRecord.apples = 10;
+someRecord.orages = 8;
+/* Below same as above */
+const someRecord2 = {};
+someRecord.apples = 10;
+someRecord.orages = 8;
